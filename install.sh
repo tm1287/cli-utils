e83b12e3c35e1415d8c689451ab4a32c57e4395e
tm1287 <tejas.maraliga@gmail.com>
2020-08-24 02:34:52 -0400

    Initial commit with new OS app install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt install -y vim
+sudo apt install -y git
+sudo apt install -y tmux
+wget -O atom.deb https://atom.io/download/deb
+sudo apt install -y ./atom.deb
+rm -rf ./atom.deb
+sudo apt install -y python-pip
+sudo apt install -y npm
