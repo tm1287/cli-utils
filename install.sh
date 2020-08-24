c13df78182838cc5d05cdae2e9e4429c5b2270d8
tm1287 <tejas.maraliga@gmail.com>
2020-08-24 02:42:41 -0400

    Add aws cli and amplify cli

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@ sudo apt install -y git
 sudo apt install -y tmux
 wget -O atom.deb https://atom.io/download/deb
 sudo apt install -y ./atom.deb
-rm -rf ./atom.deb
 sudo apt install -y python-pip
 sudo apt install -y npm
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws configure
+rm -rf ./awscliv2.zip
+rm -rf ./aws
+sudo npm install -g @aws-amplify/cli
+amplify configure
